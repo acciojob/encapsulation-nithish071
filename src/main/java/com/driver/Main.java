3092711abe86dc312e92343b86b5d57d7cd47664
nithish071 <nitheeshkumar124@gmail.com>
2024-04-11 08:00:43 +0530

    Implemented

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly rwOnly = new RWOnly();
+        // not able to access the private variable using '.' operator
+        rwOnly.setName("Nithish");
+        String name = rwOnly.getName();
+        System.out.println(name);
+    }
 }
